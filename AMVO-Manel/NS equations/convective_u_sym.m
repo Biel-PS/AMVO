e5a42127407fe898c55f933878d3c0e66f6fb4eb
Biel Pujadas Suriol <66383311+Biel-PS@users.noreply.github.com>
2025-10-21 23:24:24 +0200

    Start to explain code

--- AMVO-Manel/NS equations/convective_u_sym.m
+++ AMVO-Manel/NS equations/convective_u_sym.m
@@ -1,3 +1,19 @@
 function cu = convective_u_sym (u,v,x,y)
+% cu = convective_u_sym(u,v,x,y)
+% Computes the symbolic convective term of the velocity component u
+% Author: Pau Andrés Pérez, 2025
+%
+% Input:
+%   u : symbolic expression of velocity component u(x,y)
+%   v : symbolic expression of velocity component v(x,y)
+%   x, y : symbolic spatial variables
+%
+% Output:
+%   cu : symbolic convective term, cu = u*(du/dx) + v*(du/dy)
+%
+% Disclaimer:
+%   u,v,x,y are relative to this function, so the notation of
+%   the velocity field from the main code doesn't have to mach
+%   that of this function.
     cu = u*diff(u,x) + v*diff(u,y);
 end
--- AMVO-Manel/NS equations/set_velocity_field.m
+++ AMVO-Manel/NS equations/set_velocity_field.m
@@ -1,4 +1,13 @@
 function [u,v] = set_velocity_field ()
+% [u,v] = set_velocity_field ()
+% Defines a 2D symbolic velocity field (u,v)
+% Author: Pau Andrés Pérez, 2025
+% Input:
+%   None (symbolic variables x and y are created internally)
+% Output:
+%   u : velocity component in x-direction, u = cos(2πx)*sin(2πy)
+%   v : velocity component in y-direction, v = -sin(2πx)*cos(2πy)
+
     syms x y
     u = cos(2*pi*x)*sin(2*pi*y);
     v = -sin(2*pi*x)*cos(2*pi*y);
